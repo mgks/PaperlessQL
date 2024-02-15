dbb4320dbdeb45989edcf64504f9f01f6d8a4825
mgks <hello@mgks.dev>
2024-02-15 10:26:24 +0530

    config

---
+++ config.js
@@ -0,0 +1,12 @@
+// configure your pql here
+
+const pql_config = {
+    data: {
+      id: Number,
+      data: String,
+    },
+    temp_data: {
+      id: Number,
+      name: String,
+    },
+  };
